08520de1d5af98a7232951951864e4b7fa12144a
Praz314159 <praz.ramakrishna@gmail.com>
2020-01-02 11:03:58 +0530

    merged generate_keys.py and probabilistic_key_guessing.py

--- probabilstic_key_guessing.py
+++ probabilstic_key_guessing.py
@@ -6,11 +6,35 @@
 # In the end, this script shoudl return the bucket in which the correct keylength is most likely 
 # to appear 
 
-#import Kasisky_debug as kd 
-import Vigenere as vg 
-#from nltk.book import * 
+import Vigenere as vg  
 from nltk import corpus
 import sys 
+import random
+
+#We want to generate various keys of length 2-30 
+#each key length, we generate 5 random keys
+def gen_keys():
+    #generate keywords and write them into file 
+    start = 0 
+    stop = 255
+    with open("key_bank.txt", "w+", encoding = "utf-8") as key_bank: 
+        for keylen in range(2,30): 
+            #generate random key from ASCII characters --> range 0-255 
+            #generate 5 keys 
+            for keynum in range(5):
+                key = "" 
+                ASCII_ords = [random.randint(start, stop) for ch_ord in range(keylen)]
+                print(ASCII_ords) 
+
+                #convert list of ASCII orders into chars and write to key bank 
+                for ASCII in ASCII_ords: 
+                    key += chr(ASCII)
+                    
+                key_bank.write(key + "\n") 
+
+    return key_bank 
+
+key_bank = gen_keys() 
 
 # we have nltk.book texts. These are texts 1 - 10 
 # for each of these texts, we want to return the sorted factor frequency list 
