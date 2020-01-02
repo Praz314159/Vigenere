1923006a44593d3955056df2ff677aafe75e4279
Praz314159 <praz.ramakrishna@gmail.com>
2020-01-02 13:26:33 +0530

    only %56 success rate! ARGGGGGHH

--- probabilstic_key_guessing.py
+++ probabilstic_key_guessing.py
@@ -18,7 +18,7 @@ def gen_keys():
     start = 0 
     stop = 255
     with open("key_bank.txt", "w+", encoding = "utf-8") as key_bank: 
-        for keylen in range(2,30): 
+        for keylen in range(2,35): 
             #generate random key from ASCII characters --> range 0-255 
             #generate 5 keys 
             for keynum in range(5):
@@ -34,43 +34,60 @@ def gen_keys():
 
     return key_bank 
 
-key_bank = gen_keys() 
-
 # we have nltk.book texts. These are texts 1 - 10 
 # for each of these texts, we want to return the sorted factor frequency list 
 # But, first I just want to check how often I'm guessing the correct key length with the 
 # current heuristic 
+def validate_guess(keys, fileids): 
+    #for each fileid, we will want to check whether the correct guess is made
+    #for all keys
+    percent_correct = 0
+    correct_count = 0 
+    incorrect_count = 0
+    for fileid in fileids:
+        #print("PLAINTEXT: ", fileid) 
+        #setting plaintext 
+        plaintext = corpus.gutenberg.raw(fileid)
+        plaintext = plaintext[0:300]
+        #encrypting for each key 
+        for key in keys:
+            #correct = False 
+            keystream = vg.create_key_stream(key, plaintext)
+            #print(len(keystream)) 
+            ciphertext_file = vg.encrypt(keystream, plaintext)
+            ciphertext = open(ciphertext_file.name, "rt", encoding = "utf-8").read() 
+            #guessing key length 
+            cipher_match_indices, cipher_matches = vg.find_matches(ciphertext) 
+            key_length_guess = vg.guess_key_length(cipher_match_indices)
+            
+            if key_length_guess != keylens.get(key):
+                incorrect_count += 1 
+                #print("NO MATCH \n|key: ", key, "\n| guess: ", key_length_guess, "\n| true: ",\
+                #        keylens.get(key)) 
+            else: 
+                correct_count += 1
 
+    percent_correct = (correct_count)/(correct_count + incorrect_count)
+    return percent_correct 
+
+#generating random key bank 
+key_bank = gen_keys() 
+
+#creating string literal dictionary of keys in keybank                 
 keylens = {}  
 with open("key_bank.txt", "rt", encoding = "utf-8") as key_bank:
     for count, line in enumerate(key_bank):
         keylens.update({line: len(line)}) 
 
+#listifying keys 
 keys = list(keylens.keys())
-
-#for each fileid, we will want to check whether the correct guess is made
-#for all keys 
+#for key in keys:
+#    print(len(key))
+#getting list of fileids 
 fileids = corpus.gutenberg.fileids() 
-for fileid in fileids:
-    #setting plaintext 
-    plaintext = corpus.gutenberg.raw(fileid)
-    plaintext = plaintext[0:500]
-    #encrypting for each key 
-    for key in keys:
-        correct = False 
-        keystream = vg.create_key_stream(key, plaintext)
-        #print(len(keystream)) 
-        ciphertext_file = vg.encrypt(keystream, plaintext)
-        ciphertext = open(ciphertext_file.name, "rt", encoding = "utf-8").read() 
-        #guessing key length 
-        cipher_match_indices, cipher_matches = vg.find_matches(ciphertext) 
-        key_length_guess = vg.guess_key_length(cipher_match_indices)
 
-        if key_length_guess == keylens.get(key):
-            correct = True
-            print(correct, "| guess: ", key_length_guess, "| true: ", keylens.get(key), \
-                    "| plaintext: ", fileid) 
-
+percent_correct = validate_guess(keys, fileids) 
+print(percent_correct) 
 
 #ideally, we are able to create a plot that has key length on the x axis and percent correct guesses
 #on the y axis 
